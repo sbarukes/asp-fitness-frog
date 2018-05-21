5fb4ff32b5151790170b18daecd7602e609d8489
sbarukes <sbarukes@gmail.com>
2018-05-21 11:11:08 -0400

    added validation rules using data annotations to notes field

--- src/Treehouse.FitnessFrog/Models/Entry.cs
+++ src/Treehouse.FitnessFrog/Models/Entry.cs
@@ -89,6 +89,7 @@ namespace Treehouse.FitnessFrog.Models
         /// <summary>
         /// The notes for the entry.
         /// </summary>
+        [MaxLength(200)]
         public string Notes { get; set; }
     }
 }
